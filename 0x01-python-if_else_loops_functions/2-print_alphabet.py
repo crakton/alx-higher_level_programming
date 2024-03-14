0a0135abc679848117a726cfea2fecdf467549f9
Redemption <crakton@localhost.localdomain>
2024-03-14 10:49:56 +0100

    fix alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-# Print the lowercase alphabet without newline
-print("".join(chr(letter) for letter in range(97, 123)), end="")
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
