94ae05febcee09f13a29f5d7101044171b23a1f0
Redemption <crakton@localhost.localdomain>
2024-03-18 17:37:11 +0100

    sum of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from sys import argv
+    """Print the addition of all arguments."""
+    import sys
 
-    sum = 0
-    argslen = len(argv)
-
-    if (argslen == 2 and len(argv[1]) > 1000):
-        args = argv[1].split();
-    else:
-        args = argv[1:]
-
-    for arg in args:
-        sum += int(arg)
-    print("{}".format(sum))
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
