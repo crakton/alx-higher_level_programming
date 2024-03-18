09e376f7fe7bc124fc9888fe2faf084a30bde912
Redemption <crakton@localhost.localdomain>
2024-03-18 12:45:21 +0100

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,14 @@
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     from sys import argv
+
     argslen = len(argv) - 1;
     if (argslen == 1):
         print("1 argument:")
-        print("1: {}".format(argv[1]))
     elif (argslen > 1):
         print("{} arguments:".format(argslen))
-        for i in range(1,argslen +1):
-            print("{}: {}".format(i, argv[i]))
     else:
         print("0 arguments.")
+    for i in range(1,argslen +1):
+        print("{}: {}".format(i, argv[i]))
+
