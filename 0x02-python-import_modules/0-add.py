d17293364f4a59a2f4882e1dd56886f2066c17de
Redemption <crakton@localhost.localdomain>
2024-03-18 10:08:44 +0100

    add two variables: a, b

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
-from add_0 import add
+#!/usr/bin/python3
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
 
-
-print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
