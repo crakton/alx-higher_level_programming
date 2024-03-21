e87c20f02b2745f77e8d4f450122123518bc3ee9
Redemption <crakton@localhost.localdomain>
2024-03-21 05:40:37 +0100

    list replacement

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    new_list[search] = replace
+    for x in new_list:
+        i = new_list.index(x)
+        new_list[i] = replace if x == search else x
     return new_list
